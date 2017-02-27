e0846f786ea6eacc9e5682a4d4ffccb024cd7389
Eric Croskey <eric@artefactgroup.com@eric-croskey.local>
2017-02-26 17:40:19 -0800

    Adding style for header

--- header.php
+++ header.php
@@ -25,26 +25,26 @@
 	</head>
 	<body <?php body_class(); ?>>
 
+		<!-- header -->
+		<header class="header clear" role="banner">
+
+				<!-- logo -->
+				<div class="logo">
+					<a href="<?php echo home_url(); ?>">
+						<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
+					</a>
+				</div>
+				<!-- /logo -->
+
+				<!-- nav -->
+				<nav class="nav" role="navigation">
+					<?php mottoform_nav(); ?>
+				</nav>
+				<!-- /nav -->
+
+		</header>
+		<!-- /header -->
+
 		<!-- wrapper -->
 		<div class="wrapper">
-
-			<!-- header -->
-			<header class="header clear" role="banner">
-
-					<!-- logo -->
-					<div class="logo">
-						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
-							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo" class="logo-img">
-						</a>
-					</div>
-					<!-- /logo -->
-
-					<!-- nav -->
-					<nav class="nav" role="navigation">
-						<?php mottoform_nav(); ?>
-					</nav>
-					<!-- /nav -->
-
-			</header>
-			<!-- /header -->
