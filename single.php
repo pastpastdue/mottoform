32c7f3cf83dede50cf3ed9544203289cf973cbeb
Eric Croskey <eric@artefactgroup.com@eric-croskey.local>
2017-03-11 23:05:10 -0800

    Basic page template.

--- single.php
+++ single.php
@@ -9,26 +9,27 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main site-margin" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-			get_template_part( 'template-parts/content', get_post_format() );
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<?php if ( has_post_thumbnail() ) {
+				the_post_thumbnail( '4-col-horizontal' );
+			} ?>
+			<header class="entry-header">
+				<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
+			</header><!-- .entry-header -->
 
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div><!-- .entry-content -->
 
-			// // If comments are open or we have at least one comment, load up the comment template.
-			// if ( comments_open() || get_comments_number() ) :
-			// 	comments_template();
-			// endif;
+		</article><!-- #post-## -->
 
-		endwhile; // End of the loop.
-		?>
+	<?php endwhile; endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+	</main><!-- #main -->
 
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
