e30890d3c00ab77377e6a9c6179d129641d7b1ae
Eric Croskey <eric@artefactgroup.com@eric-croskey.local>
2017-03-19 21:50:08 -0700

    Update logic around image display on single.php

--- partials/3_column_image.php
+++ partials/3_column_image.php
@@ -1,12 +1,27 @@
 <ul class="product-flex-list">
   <li class="grid-3-col">
+    <?php
+
+    $three_column_horizontal_image= get_sub_field('three_column_horizontal_image');
+    $three_column_square_image= get_sub_field('three_column_square_image');
+
+
+      if( !empty($three_column_horizontal_image) ): ?>
       <?php // loop through the rows of data
-        $three_column_horizontal_image= get_sub_field('three_column_horizontal_image');
         echo '<img src="' . $three_column_horizontal_image['sizes']['3-col-horizontal'] . '" alt="' . $three_column_horizontal_image['alt'] . '" />';
       ?>
       <?php if($three_column_horizontal_image['caption']) : ?>
       <div class="product-image-description">
         <?php echo $three_column_horizontal_image['caption'] ?>
+      </div>
+      <?php endif; ?>
+      <?php endif; if( !empty($three_column_square_image) ): ?>
+      <?php // loop through the rows of data
+        echo '<img src="' . $three_column_square_image['sizes']['3-col-square'] . '" alt="' . $three_column_square_image['alt'] . '" />';
+      ?>
+      <?php if($three_column_square_image['caption']) : ?>
+      <div class="product-image-description">
+        <?php echo $three_column_square_image['caption'] ?>
       </div>
       <?php endif; ?>
   </li>
--- partials/4_column_image.php
+++ partials/4_column_image.php
@@ -1,12 +1,27 @@
 <ul class="product-flex-list">
   <li class="grid-4-col">
+    <?php
+    
+    $four_column_horizontal_image= get_sub_field('four_column_horizontal_image');
+    $four_column_square_image= get_sub_field('four_column_uncropped_image');
+
+    if( !empty($four_column_horizontal_image) ): ?>
       <?php // loop through the rows of data
-        $four_column_horizontal_image= get_sub_field('four_column_horizontal_image');
+
         echo '<img src="' . $four_column_horizontal_image['sizes']['4-col-horizontal'] . '" alt="' . $four_column_horizontal_image['alt'] . '" />';
       ?>
       <?php if($four_column_horizontal_image['caption']) : ?>
       <div class="product-image-description">
         <?php echo $four_column_horizontal_image['caption'] ?>
+      </div>
+      <?php endif; ?>
+    <?php endif; if( !empty($four_column_square_image) ): ?>
+      <?php // loop through the rows of data
+        echo '<img src="' . $four_column_square_image['sizes']['4-col-horizontal'] . '" alt="' . $four_column_square_image['alt'] . '" />';
+      ?>
+      <?php if($four_column_square_image['caption']) : ?>
+      <div class="product-image-description">
+        <?php echo $four_column_square_image['caption'] ?>
       </div>
       <?php endif; ?>
   </li>
