77a429de57a8481849c097e755187e3b61a6ee7b
Eric Croskey <eric@artefactgroup.com@eric-croskey.local>
2017-03-19 21:53:03 -0700

    Update logic around image display on single.php

--- partials/4_column_image.php
+++ partials/4_column_image.php
@@ -1,7 +1,7 @@
 <ul class="product-flex-list">
   <li class="grid-4-col">
     <?php
-    
+
     $four_column_horizontal_image= get_sub_field('four_column_horizontal_image');
     $four_column_square_image= get_sub_field('four_column_uncropped_image');
 
@@ -23,6 +23,6 @@
       <div class="product-image-description">
         <?php echo $four_column_square_image['caption'] ?>
       </div>
-      <?php endif; ?>
+    <?php endif; endif;?>
   </li>
 </ul>
