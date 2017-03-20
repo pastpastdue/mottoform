55261a3c6c3a865ea6524ce0168d3d11ca165ad7
Eric Croskey <eric@artefactgroup.com@eric-croskey.local>
2017-03-19 22:04:24 -0700

    Update logic around image display on single.php

--- partials/3_column_image.php
+++ partials/3_column_image.php
@@ -6,15 +6,17 @@
     $three_column_square_image= get_sub_field('three_column_square_image');
 
 
-      if( !empty($three_column_horizontal_image) ): ?>
-      <?php // loop through the rows of data
+      if( !empty($three_column_horizontal_image) ):
+
         echo '<img src="' . $three_column_horizontal_image['sizes']['3-col-horizontal'] . '" alt="' . $three_column_horizontal_image['alt'] . '" />';
-      ?>
-        <?php if($three_column_horizontal_image['caption']) : ?>
+
+        if($three_column_horizontal_image['caption']) : ?>
+        
         <div class="product-image-description">
           <?php echo $three_column_horizontal_image['caption'] ?>
         </div>
         <?php endif; ?>
+
       <?php endif; if( !empty($three_column_square_image) ): ?>
       <?php // loop through the rows of data
         echo '<img src="' . $three_column_square_image['sizes']['3-col-square'] . '" alt="' . $three_column_square_image['alt'] . '" />';
