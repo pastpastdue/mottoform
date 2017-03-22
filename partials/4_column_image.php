7ae686a521764d28095e4b3aa75528177f5c62b0
Eric Croskey <eric@artefactgroup.com@eric-croskey.local>
2017-03-22 16:39:30 -0700

    Change all displays for correct display

--- partials/4_column_image.php
+++ partials/4_column_image.php
@@ -6,24 +6,25 @@
     $four_column_square_image= get_sub_field('four_column_uncropped_image');
 
     if( !empty($four_column_horizontal_image) ):
-
         echo '<img src="' . $four_column_horizontal_image['sizes']['4-col-horizontal'] . '" alt="' . $four_column_horizontal_image['alt'] . '" />';
-
-      if($four_column_horizontal_image['caption']) : ?>
+      ?>
       <div class="product-image-description">
+      <?php if($four_column_horizontal_image['caption']) : ?>
         <?php echo $four_column_horizontal_image['caption'] ?>
-      </div>
       <?php endif; ?>
-    <?php endif;
-    if( !empty($four_column_square_image) ): ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if( !empty($four_column_square_image) ): ?>
       <?php // loop through the rows of data
         echo '<img src="' . $four_column_square_image['sizes']['4-col-horizontal'] . '" alt="' . $four_column_square_image['alt'] . '" />';
       ?>
       <div class="product-image-description">
       <?php if($four_column_square_image['caption']) : ?>
         <?php echo $four_column_square_image['caption'] ?>
-      <?php endif; ?>&nbsp;
+      <?php endif; ?>
       </div>
     <?php endif; ?>
+
   </li>
 </ul>
