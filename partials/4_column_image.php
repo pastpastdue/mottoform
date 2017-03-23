66daeddb2b2870e959df0b242e4c874eaf4e19ba
Eric Croskey <eric@artefactgroup.com@eric-croskey.corp.artefact.com>
2017-03-23 14:44:13 -0700

    Change 4 col image display:

--- partials/4_column_image.php
+++ partials/4_column_image.php
@@ -17,7 +17,7 @@
 
     <?php if( !empty($four_column_square_image) ): ?>
       <?php // loop through the rows of data
-        echo '<img src="' . $four_column_square_image['sizes']['4-col-horizontal'] . '" alt="' . $four_column_square_image['alt'] . '" />';
+        echo '<img src="' . $four_column_square_image['sizes']['4-col-square'] . '" alt="' . $four_column_square_image['alt'] . '" />';
       ?>
       <div class="product-image-description">
       <?php if($four_column_square_image['caption']) : ?>
