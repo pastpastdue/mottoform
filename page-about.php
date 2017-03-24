1e64582e9361e2a82466caa77df64bd80e9ff9ed
Eric Croskey <eric@artefactgroup.com@eric-croskey.corp.artefact.com>
2017-03-23 17:21:06 -0700

    Update page to display custom fields opn about

--- page-about.php
+++ page-about.php
@@ -66,17 +66,23 @@ get_header(); ?>
 				<div class="grid-2-col col-float-left">
 					<div class="page-additional-text entry-content">
 						<h4>Awards</h4>
-						<?php the_sub_field('awards'); ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+							<?php the_sub_field('awards'); ?>
+						<?php endwhile; ?>
 					</div>
 					<div class="page-additional-text entry-content">
 						<h4>Grants</h4>
-						<?php the_sub_field('grants'); ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+							<?php the_sub_field('grants'); ?>
+						<?php endwhile; ?>
 					</div>
 				</div>
 				<div class="grid-2-col col-float-right">
 					<div class="page-additional-text entry-content">
 						<h4>SELECT DESIGNS/PROJECTS</h4>
-						<?php the_sub_field('select'); ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+							<?php the_sub_field('select_designs'); ?>
+						<?php endwhile; ?>
 					</div>
 				</div>
 			</div>
