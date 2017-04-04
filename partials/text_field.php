a319cfaed814bdf772215630f1f3a6501e4d85c6
Eric Croskey <eric@artefactgroup.com@eric-croskey.local>
2017-04-03 22:30:23 -0700

    Adding image fixes

--- partials/text_field.php
+++ partials/text_field.php
@@ -1,4 +1,3 @@
-<div class="product-additional-text entry-content <?php
-if($one_column_image['caption'] || $two_column_uncropped['caption'] || $three_column_horizontal_image['caption'] || $three_column_square_image['caption'] || $four_column_horizontal_image['caption'] || $four_column_square_image['caption'] ) :?> caption-margin-text <?php endif; ?> ">
+<div class="product-additional-text entry-content <?php if($one_column_image['caption'] || $two_column_uncropped['caption'] || $three_column_horizontal_image['caption'] || $three_column_square_image['caption'] || $four_column_horizontal_image['caption'] || $four_column_square_image['caption'] ) :?> caption-margin-text <?php endif; ?> ">
   <?php the_sub_field('additional_text_description'); ?>
 </div>
